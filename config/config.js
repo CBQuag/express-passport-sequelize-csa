d239ea1465bb3de165cfeb2b41e7dfdd74a0c09c
Conrad Quagliaroli <conradbquag@gmail.com>
2023-05-01 11:29:23 -0400

    removed host name from production

--- config/config.js
+++ config/config.js
@@ -26,7 +26,5 @@ module.exports = {
     "username": process.env.DATABASE_USER,
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
-    "host": "127.0.0.1",
-    "dialect": "postgres"
   }
 }
