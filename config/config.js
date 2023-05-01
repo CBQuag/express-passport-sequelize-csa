bc079d9369d23a51d31e26270053176cb4a76f5c
Conrad Quagliaroli <conradbquag@gmail.com>
2023-05-01 11:44:17 -0400

    added DEVELOPMENT_ back to development

--- config/config.js
+++ config/config.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 
 module.exports = {
   "development": {
-    "username": process.env.DATABASE_USER,
-    "password": process.env.DATABASE_PASSWORD, //probably null
-    "database": process.env.DATABASE_NAME,
+    "username": process.env.DEVELOPMENT_DATABASE_USER,
+    "password": process.env.DEVELOPMENT_DATABASE_PASSWORD, //probably null
+    "database": process.env.DEVELOPMENT_DATABASE_NAME,
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
