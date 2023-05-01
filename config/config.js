6218e8e6fafd081aeb4be65f2940b9e7dd513f00
Conrad Quagliaroli <conradbquag@gmail.com>
2023-05-01 11:19:20 -0400

    removed DEVELOPMENT_ from env

--- config/config.js
+++ config/config.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 
 module.exports = {
   "development": {
-    "username": process.env.DEVELOPMENT_DATABASE_USER,
-    "password": process.env.DEVELOPMENT_DATABASE_PASSWORD, //probably null
-    "database": process.env.DEVELOPMENT_DATABASE_NAME,
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASSWORD, //probably null
+    "database": process.env.DATABASE_NAME,
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
@@ -23,9 +23,9 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "username": process.env.DEVELOPMENT_DATABASE_USER,
-    "password": process.env.DEVELOPMENT_DATABASE_PASSWORD,
-    "database": process.env.DEVELOPMENT_DATABASE_NAME,
+    "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_NAME,
     "host": "127.0.0.1",
     "dialect": "postgres"
   }
