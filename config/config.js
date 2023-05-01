b63f444875bd3d3ea57f40875a7eeb0c1f546a51
Conrad Quagliaroli <conradbquag@gmail.com>
2023-05-01 11:10:54 -0400

    changed production on config

--- config/config.js
+++ config/config.js
@@ -23,9 +23,9 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "username": "conradquagliaroli",
-    "password": null,
-    "database": "database_production",
+    "username": process.env.DEVELOPMENT_DATABASE_USER,
+    "password": process.env.DEVELOPMENT_DATABASE_PASSWORD,
+    "database": process.env.DEVELOPMENT_DATABASE_NAME,
     "host": "127.0.0.1",
     "dialect": "postgres"
   }
